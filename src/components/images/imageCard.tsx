f39c22bbd1af84fb95e0c55c2cbfcea123e109a5
Seba <tudora.sebastian96@gmail.com>
2025-02-14 12:02:14 +0200

    AddedPlaceholderForImages

--- src/components/images/imageCard.tsx
+++ src/components/images/imageCard.tsx
@@ -6,8 +6,10 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import { Image } from "@/types/Image.types";
-
+import { useState } from "react";
+import Placeholder from "../../assets/placeholder.png.webp";
 const ImageCard: React.FC<Image> = (props) => {
+	const [loading, setLoading] = useState(true);
 	return (
 		<Card>
 			<CardHeader>
@@ -20,8 +22,9 @@ const ImageCard: React.FC<Image> = (props) => {
 				<img
 					alt={props.title}
 					loading="lazy"
+					onLoad={() => setLoading(false)}
 					className="object-cover w-[100%] h-[250px] md:w-[300px] md:h-[300px] lg:w-[500px] lg:h-[500px] max-w-[500px]  rounded-sm "
-					src={props.hdurl ?? props.thumbnail_url}
+					src={loading ? Placeholder : (props.hdurl ?? props.thumbnail_url)}
 				/>
 			</CardContent>
 		</Card>
